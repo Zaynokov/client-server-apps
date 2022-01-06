c72fe25e0d2083a9f1d21c9c501aa717c44b2d09
Zaynokov <vlerii99@mail.ru>
2022-01-06 18:12:11 +0300

    lesson 5

--- app/log/server_log_config.py
+++ app/log/server_log_config.py
@@ -3,7 +3,7 @@ import logging
 app_log = logging.getLogger('app')
 app_log.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('server.log')
+file_handler = logging.FileHandler('log/server.log')
 file_format = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(message)s')
 file_handler.setFormatter(file_format)
 file_handler.setLevel(logging.INFO)
